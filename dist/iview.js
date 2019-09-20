1ba6e5a4becdb288c95ed77e283814e1efd1e26f
yangd <y@d.com>
2019-09-20 17:08:13 +0800

    升级版本

--- dist/iview.js
+++ dist/iview.js
@@ -28819,7 +28819,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 var API = (0, _extends3.default)({
-    version: '3.5.1-pi.2',
+    version: '3.5.2-pi.1',
     locale: _index2.default.use,
     i18n: _index2.default.i18n,
     install: install,
