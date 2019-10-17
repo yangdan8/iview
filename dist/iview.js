9d7f800bf10178aa77e6cd1317dead37206b1172
yangd <y@d.com>
2019-10-17 17:12:08 +0800

    重新发布

--- dist/iview.js
+++ dist/iview.js
@@ -28833,7 +28833,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 var API = (0, _extends3.default)({
-    version: '3.5.2-pi.3',
+    version: '3.5.2-pi.4',
     locale: _index2.default.use,
     i18n: _index2.default.i18n,
     install: install,
