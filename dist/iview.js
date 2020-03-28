7929c78a4b31896ed258c73e1dbf093dbd3bd0f5
yangd <y@d.com>
2020-03-28 18:48:21 +0800

    更新dts报错

--- dist/iview.js
+++ dist/iview.js
@@ -29155,7 +29155,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 var API = (0, _extends3.default)({
-    version: '3.5.5-pi-8',
+    version: '3.5.5-pi-9',
     locale: _index2.default.use,
     i18n: _index2.default.i18n,
     install: install,
